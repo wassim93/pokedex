9d03f217802dedf41c69e35b6f4d6cb41ef2a360
wassim <b.wassim1993@hotmail.fr>
2021-09-07 02:31:48 +0100

    adding floating button

--- Pokedex/View/PokedexView.swift
+++ Pokedex/View/PokedexView.swift
@@ -11,21 +11,47 @@ struct PokedexView: View {
     
     private let gridItems :[GridItem] = Array(repeating: .init(.flexible(),spacing:3), count: 2)
     @ObservedObject var pvModel :PokemonViewModel = PokemonViewModel()
-
+    
     var body: some View {
-        NavigationView{
-            ScrollView{
-                LazyVGrid(columns: gridItems, spacing: 16) {
-                    ForEach(pvModel.pokemon) { pokemon in
-                        NavigationLink(destination: DetailView(pokemon: pokemon, viewModel: pvModel)) {
-                            PokemonCell(pokemon: pokemon, viewModel: pvModel)
+        ZStack{
+            NavigationView{
+                ScrollView{
+                    LazyVGrid(columns: gridItems, spacing: 16) {
+                        ForEach(pvModel.pokemon) { pokemon in
+                            NavigationLink(destination: DetailView(pokemon: pokemon, viewModel: pvModel)) {
+                                PokemonCell(pokemon: pokemon, viewModel: pvModel)
+                            }
                         }
                     }
                 }
-            }
-            .navigationTitle("Pokemon")
-        }.accentColor(.white)
+                .navigationTitle("Pokemon")
+            }.accentColor(.white)
+            
+            FloatingButtonView()
+        }
     }
+    
+    fileprivate func FloatingButtonView() -> some View{
+        return VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {
+                    print("click")
+                }, label: {
+                    Image(systemName: "list.bullet.indent")
+                        .frame(width: 67, height: 60)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 7)
+                })
+                .background(Color.purple)
+                .cornerRadius(38.5)
+                .padding()
+                .shadow(color: Color.black.opacity(0.3),radius: 3,x: 3,y: 3)
+            }
+        }
+    }
+    
 }
 
 struct PokedexView_Previews: PreviewProvider {
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pokedex
+
+SwiftUI Project 
+ - Showing List of pokemon based onAPI Response
+ - Showing Pokemon details
+ - Filtering Pokemon
+MVVM Architecture
+
+Library used : Kingfisher  
